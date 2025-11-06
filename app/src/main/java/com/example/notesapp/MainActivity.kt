f2f63529c1daa7eb96e05058da8f503858c45524
pikimen357 <vidkyadhipradana2004@mail.ugm.ac.id>
2025-11-06 09:53:34 +0700

    NoteRowDeletable

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -13,6 +15,7 @@ import androidx.compose.ui.unit.dp
 
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.room.Delete
 import com.example.notesapp.Note
 import com.example.notesapp.NotesViewModel
 
@@ -58,13 +61,25 @@ fun NotesScreen(vm: NotesViewModel = viewModel()) {
         Spacer(Modifier.height(12.dp))
         LazyColumn {
             items(notes, key = { it.id }) { note ->
-                NoteRowReadOnly(note = note)
+//                NoteRowReadOnly(note = note)
+                NoteRowDeletable(note = note, onDelete = {vm.deleteNote(it)})
                 Divider()
             }
         }
     }
 }
 
+@Composable
+fun NoteRowDeletable(note: Note, onDelete: (Note) -> Unit){
+    ListItem(
+        headlineContent = {Text(note.title)},
+        trailingContent = {
+            IconButton(onClick = {onDelete(note)}) {
+                Icon(Icons.Filled.Delete, contentDescription = "Hapus")
+            }
+        }
+    )
+}
 
 @Composable
 fun NoteRowReadOnly(note: Note) {
